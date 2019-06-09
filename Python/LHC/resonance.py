6deb21e88541e014ef888e92b168ab4cacc0248a
RoccoA97 <roccoardino@gmail.com>
2019-06-10 00:27:32 +0200

    update

--- Python/LHC/resonance.py
+++ Python/LHC/resonance.py
@@ -12,30 +12,34 @@ def delta(omega, omega_0, gamma, Gamma=1):
     return np.arctan((2*gamma*omega_0*omega)/(omega_0**2 - omega**2))
 
 omega_0 = 1.0
-gamma = 0.2
+gamma = [0.1,0.15,0.2,0.25,0.3]
 Gamma = 1.0
 
 x1 = np.arange(0.0, 2.0, 0.01)
 x2 = np.arange(-np.pi/2, np.pi/2, 0.01)
-y2 = delta(t1,omega_0,gamma,Gamma)
-
-xy = [t1]
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-fig = plt.figure(1, figsize=(10, 4))
+fig = plt.figure(1, figsize=(12, 5))
 plt.subplot(121)
 plt.title('Amplitude B')
-plt.xlabel('$\omega_0$')
+plt.xlabel('$\Omega$')
 plt.ylabel('B')
-plt.plot(x1, B(x1,omega_0, gamma, Gamma))
+for i in range(len(gamma)):
+    label = '$\gamma$ = ' + str(gamma[i])
+    print(label)
+    plt.plot(x1, B(x1,omega_0, gamma[i], Gamma),label=label)
+plt.legend()
 
 plt.subplot(122)
 plt.title('Phase shift $\delta$')
-plt.xlabel('$\omega_0$')
+plt.xlabel('$\Omega$')
 plt.ylabel('$\delta$')
-plt.plot(x1, delta(x1,omega_0, gamma, Gamma))
+for i in range(len(gamma)):
+    label = '$\gamma$ = ' + str(gamma[i])
+    plt.plot(x1, delta(x1,omega_0, gamma[i], Gamma),label=label)
+plt.legend()
 plt.show()
 
 os.getcwd()
