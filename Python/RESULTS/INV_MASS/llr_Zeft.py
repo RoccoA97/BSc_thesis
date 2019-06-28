62cad081087af0e4d586f3b36b3e83910a703c74
RoccoA97 <roccoardino@gmail.com>
2019-06-28 19:41:55 +0200

    update

--- Python/RESULTS/INV_MASS/llr_Zeft.py
+++ Python/RESULTS/INV_MASS/llr_Zeft.py
@@ -8,8 +8,12 @@ from scipy.stats import chi2, chisquare
 from scipy.special import erfinv
 
 
-toys = [5, 9, 19, 20]
+toys = [3, 6, 9, 12,
+        15, 18, 21, 24,
+        27, 30, 33, 36,
+        39, 42, 45, 48]
 verbose = 1
+fontsize = 13
 
 print(len(toys))
 FILE_PATH = 'C:\\Users\\Ardino Pierfrancesco\\Desktop\\Z_5D_SIMULATIONS_RESULTS-master\\wcliptest\\ref300000_bkg0_eft20000\\epochs300000\\Z_5D_patience5000_ref300000_bkg0_eft20000_epochs300000_latent5_wclip2.45_run1\\'
@@ -120,36 +124,25 @@ def __read_from_h5_file_predictions__(FILE_PATH, toys):
 # - mass
 predictions = __read_from_h5_file_predictions__(FILE_PATH, toys)
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+fig=plt.figure(figsize=(20, 16))
+plt.clf()
+
 for i in range(len(toys)):
     print(i)
-    plt.clf()
-    fig=plt.figure(figsize=(18, 4.5))
-    plt.subplot(1,3,1)
-    print(type(predictions[i][5][predictions[i][2]==1]))
-    print(predictions[i][5][predictions[i][2]==1].shape)
+    plt.subplot(4,4,i+1)
     x = predictions[i][5][predictions[i][2]==1][:,0]
     y = predictions[i][0][predictions[i][2]==1][:,0]
-    print(type(x))
-    print(x.shape)
-    print(y.shape)
-    plt.xlabel('$M_Z$')
-    plt.ylabel('$f(M_Z)$')
-    plt.hist2d(x,y, bins=100, range=[[0, 800], [-0.25, 2.5]], norm=colors.PowerNorm(0.1))
-    #plt.plot(predictions[i][5][predictions[i][2]==1],predictions[i][0][predictions[i][2]==1],'.')
-    #plt.xlim(0,350)
 
-    data_mass = predictions[i][5][predictions[i][2]==1]
-    data_mass_ref = predictions[i][5][predictions[i][2]==0]
+    plt.title('NN output $f(M_Z)$ - Toy ' + str(toys[i]), fontsize=fontsize)
+    plt.xlabel('$M_Z$', fontsize=fontsize)
+    plt.ylabel('$f(M_Z)$', fontsize=fontsize)
+    plt.hist2d(x,y, bins=100, range=[[0, 800], [-0.25, 5.0]], norm=colors.PowerNorm(0.05))
 
-    plt.subplot(1,3,2)
-    plt.hist(data_mass, bins=100)#, #range=(250,350))#, density=True)
-    plt.yscale('log')
-
-
-    plt.subplot(1,3,3)
-    plt.hist(data_mass_ref, bins=100)#, #range=(250,350))#, density=True)
-    plt.yscale('log')
-
-    plt.show()
-    plt.clf()
-    fig.clf()
+fig.subplots_adjust(left = 0.03,right = 0.98,bottom = 0.03,top = 0.98,hspace = 0.30)
+fig.savefig('./Python/RESULTS/INV_MASS/f_plot_Zeft.pdf')
+plt.show()
+plt.clf()
+fig.clf()
--- Python/RESULTS/INV_MASS/llr_Zprime.py
+++ Python/RESULTS/INV_MASS/llr_Zprime.py
@@ -8,8 +8,12 @@ from scipy.stats import chi2, chisquare
 from scipy.special import erfinv
 
 
-toys = [5, 9, 19, 20]
+toys = [5, 9, 16, 18,
+        19, 20, 21, 23,
+        26, 40, 43, 44,
+        45, 47, 48, 51]
 verbose = 1
+fontsize = 13
 
 print(len(toys))
 FILE_PATH = 'C:\\Users\\Ardino Pierfrancesco\\Desktop\\Z_5D_SIMULATIONS_RESULTS-master\\wcliptest\\ref1000000_bkg20000_sig40\\epochs300000\\Z_5D_patience5000_ref1000000_bkg20000_sig40_epochs300000_latent5_wclip2.7_run1\\'
@@ -64,11 +68,15 @@ def __read_from_h5_file_predictions__(FILE_PATH, toys):
 # - mass
 predictions = __read_from_h5_file_predictions__(FILE_PATH, toys)
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+fig=plt.figure(figsize=(20, 16))
+plt.clf()
+
 for i in range(len(toys)):
     print(i)
-    plt.clf()
-    fig=plt.figure(figsize=(18, 4.5))
-    plt.subplot(1,3,1)
+    plt.subplot(4,4,i+1)
     print(type(predictions[i][5][predictions[i][2]==1]))
     print(predictions[i][5][predictions[i][2]==1].shape)
     x = predictions[i][5][predictions[i][2]==1][:,0]
@@ -76,12 +84,15 @@ for i in range(len(toys)):
     print(type(x))
     print(x.shape)
     print(y.shape)
-    plt.xlabel('$M_Z$')
-    plt.ylabel('$f(M_Z)$')
-    plt.hist2d(x,y, bins=100, range=[[0, 350], [-0.25, 2.5]], norm=colors.PowerNorm(0.05))
+
+    plt.title('NN output $f(M_Z)$ - Toy ' + str(toys[i]), fontsize=fontsize)
+    plt.xlabel('$M_Z$', fontsize=fontsize)
+    plt.ylabel('$f(M_Z)$', fontsize=fontsize)
+    plt.hist2d(x,y, bins=100, range=[[0, 400], [-0.25, 3.0]], norm=colors.PowerNorm(0.05))
     #plt.plot(predictions[i][5][predictions[i][2]==1],predictions[i][0][predictions[i][2]==1],'.')
     #plt.xlim(0,350)
 
+    """
     data_mass = predictions[i][5][predictions[i][2]==1]
     data_mass_ref = predictions[i][5][predictions[i][2]==0]
 
@@ -93,7 +104,10 @@ for i in range(len(toys)):
     plt.subplot(1,3,3)
     plt.hist(data_mass_ref, bins=100)#, #range=(250,350))#, density=True)
     plt.yscale('log')
+    """
 
-    plt.show()
-    plt.clf()
-    fig.clf()
+fig.subplots_adjust(left = 0.03,right = 0.98,bottom = 0.03,top = 0.98,hspace = 0.30)
+fig.savefig('./Python/RESULTS/INV_MASS/f_plot_Zprime.pdf')
+plt.show()
+plt.clf()
+fig.clf()
